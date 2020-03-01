cf01443446a0caa08b3e4a4fd7742fa0b0981d4c
Anthony Mai <anthony.mai530@gmail.com>
2020-03-01 20:16:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,13 +32,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(username: params[:username], password: params[:password])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/account"
-    else
-      redirect "/failure"
-    end
+    
   end
 
   get "/failure" do
