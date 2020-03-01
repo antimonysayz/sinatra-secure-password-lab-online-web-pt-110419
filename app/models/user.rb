7eb3e5dd2d6d10c0c6f867f6fd1ad4fb887cdac9
Anthony Mai <anthony.mai530@gmail.com>
2020-03-01 20:20:15 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,6 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   include BCrypt
 
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
-  end
+  has_secure_password
 
 end
